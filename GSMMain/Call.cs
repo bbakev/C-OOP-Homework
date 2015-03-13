8e554864b1d73835d93ee181a71e6f88682df8f1
bbakev <bakevfcb@abv.bg>
2015-03-13 12:26:01 +0200

    The Last

---
+++ GSMMain/Call.cs
@@ -0,0 +1,27 @@
+﻿namespace GSMMain
+{
+    using System;
+
+    public class Call
+    {
+        private DateTime date;
+        private string dialedNumber;
+        private int duration;
+
+        public Call (DateTime date, string dialedNumber, int duration)
+        {
+            this.Date = date;
+            this.DialedNumber = dialedNumber;
+            this.Duration = duration;
+        }
+        
+        public DateTime Date { get; set;}
+        public string DialedNumber { get; set; }
+        public int Duration { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Date and time: {0:G}; Number dialed: {1}; Call duration: {2}", this.Date, this.DialedNumber, this.Duration); 
+        }
+    }
+}
---
+++ GSMMain/GSMCallHistoryTest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace GSMMain
+{
+    
+
+    public class GSMCallHistoryTest
+    {
+        private const decimal charge = (decimal)0.37;
+
+        public void HistoryTest ()
+        {
+            GSM firstPhone = new GSM("N100", "Nokia", 100.98, "Pesho");
+            firstPhone.AddCall(new Call(DateTime.Parse("12/02/2015 19:23:34"), "388297912", 435));
+            firstPhone.AddCall(new Call(DateTime.Parse("15/03/2015 14:22:31"), "388297912654", 60));
+            firstPhone.AddCall(new Call(DateTime.Parse("23/03/2015 21:45:21"), "5466388297912", 100));
+            firstPhone.AddCall(new Call(DateTime.Parse("23/03/2015 22:45:21"), "38845244597912", 126));
+
+            Console.WriteLine(firstPhone.PrintCallHistory());
+
+            Console.WriteLine("The total price of the calls in the call history is: {0}.", firstPhone.TotalCostOfCalls(charge));
+
+
+            Call longestCall = firstPhone.CallHistory.OrderBy(x => x.Duration).ToArray()[firstPhone.CallHistory.Count - 1];
+            firstPhone.RemoveCall(longestCall);
+            
+           
+            Console.WriteLine(firstPhone.PrintCallHistory());
+        }      
+
+    }
+}
